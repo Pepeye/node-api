e9b04e6acf35e1a8de96be20fcb358c68e439a32
Pepeye <gaofolaju@gmail.com>
2017-02-18 16:50:09 +0000

    add POST /todos route

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
 const express = require('express')
-const bodyparser = require('body-parser')
+const bodyParser = require('body-parser')
+const logger = require('morgan')
+let debug = require('debug')('stormtrooper')
 
 // const mongoose = require('./lib/db')
-// const { Todo, User } = require('./models')
+const { Todo } = require('./models')
 
 let app = express()
 
-app.use(bodyparser.json())
+/**
+ * Configure middleware
+ */
+debug('configuring middleware')
+app.use(logger('dev'))
+app.use(bodyParser.json())
 
+/**
+ * API routes / endpoints
+ */
 app.use('/todos', (req, res) => {
-  console.log(req.body)
+  // console.log(req.body)
+  let todo = new Todo({
+    text: req.body.text
+  })
+  todo
+    .save()
+    .then((doc) => res.send(doc))
+    .catch(err => res.status(400).send(err))
 })
 
+// console.dir(debug, { colors: true, depth: 2 })
 app.listen(3000, () => {
-  console.log('Started on port 3000')
+  debug('Server started on port 3000')
 })
--- src/models/index.js
+++ src/models/index.js
@@ -1,8 +1,6 @@
 const User = require('./user')
 const Todo = require('./todo')
 
-console.log('[info]: requiring models')
-
 module.exports = {
   User,
   Todo
