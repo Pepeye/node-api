3bca8cc58280a525e407807f6f951bc6923953f6
Pepeye <gaofolaju@gmail.com>
2017-02-18 18:01:08 +0000

    added POST /todos tests

--- build/tasks/seed.js
+++ build/tasks/seed.js
@@ -1,26 +1,28 @@
 // const mongoose = require('../../src/lib/db')
 const seed = require('../../src/lib/seed')
 const { Todo, User } = require('../../src/models')
-const { jsonprint } = require('../../src/lib/helpers')
+const { jsonprint } = require('../../src/lib/helpers');
 
-User
-  .insertMany(seed.users)
-  .then(docs => jsonprint(docs))
-  .catch(err => console.log(err))
+(() => {
+  User
+    .insertMany(seed.users)
+    .then(docs => jsonprint(docs))
+    .catch(err => console.log(err))
 
-// let todo = new Todo({
-//   text: 'Learn Mongoose ORM',
-//   completed: false
-// })
+  // let todo = new Todo({
+  //   text: 'Learn Mongoose ORM',
+  //   completed: false
+  // })
 
-// insert a single todo item
-// todo
-//   .save()
-//   .then(doc => jsonprint(doc))
-//   .catch(err => console.log(err))
+  // insert a single todo item
+  // todo
+  //   .save()
+  //   .then(doc => jsonprint(doc))
+  //   .catch(err => console.log(err))
 
-// insert multiple todo items
-Todo
-  .insertMany(seed.todos)
-  .then(docs => jsonprint(docs))
-  .catch(err => console.log(err))
+  // // insert multiple todo item
+  Todo
+    .insertMany(seed.todos)
+    .then(docs => jsonprint(docs))
+    .catch(err => console.log(err))
+})()
--- src/tests/server.test.js
+++ src/tests/server.test.js
@@ -2,29 +2,62 @@ const request = require('supertest')
 const { app } = require('../')
 const { Todo } = require('../models')
 
-describe('POST /todos', () => {
-  test('should create a new todo', (done) => {
-    let text = 'Todo test'
-
-    request(app)
-      .post('/todos')
-      .send({ text })
-      .expect(200)
-      .expect((res) => {
-        expect(res.body.text).toBe(text)
+describe('STORMTROOPER TESTS SUITE', () => {
+  beforeAll((done) => {
+    Todo
+      .remove({})
+      .then(({ result }) => {
+        console.log(`[info]: removed collection ${Todo.modelName} - ${result.n} items removed.`)
+        done()
       })
-      .end((err, res) => {
-        if (err) {
-          return done(err)
-        }
+      .catch(err => done(err))
+  })
 
-        Todo
-          .find({ text })
-          .then((todos) => {
-            expect(todos.length).toBe(1)
-            expect(todos[0].text).toBe(text)
-            done()
-          })
-      })
+  describe('POST /todos', () => {
+    test('should create a new todo', (done) => {
+      let text = 'Todo test'
+
+      request(app)
+        .post('/todos')
+        .send({ text })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.text).toBe(text)
+        })
+        .end((err, res) => {
+          if (err) {
+            return done(err)
+          }
+
+          Todo
+            .find()
+            .then((todos) => {
+              expect(todos.length).toBe(1)
+              expect(todos[0].text).toBe(text)
+              done()
+            })
+            .catch(err => done(err))
+        })
+    })
+
+    test('should not create todo with invalid data', (done) => {
+      request(app)
+        .post('/todos')
+        .send({})
+        .expect(400)
+        .end((err, res) => {
+          if (err) {
+            return done(err)
+          }
+
+          Todo
+            .find()
+            .then((todos) => {
+              expect(todos.length).toBe(0)
+              done()
+            })
+            .catch(err => done(err))
+        })
+    })
   })
 })
