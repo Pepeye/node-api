8b41e2e1cc9ed9d7bf4f5454da381d1b2307a182
Pepeye <gaofolaju@gmail.com>
2017-02-18 17:01:58 +0000

    refractored server + added test

--- src/index.js
+++ src/index.js
@@ -1,35 +1,17 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const logger = require('morgan')
-let debug = require('debug')('stormtrooper')
+const app = require('./server')
+const debug = require('debug')('stormtrooper:server')
 
-// const mongoose = require('./lib/db')
-const { Todo } = require('./models')
-
-let app = express()
-
-/**
- * Configure middleware
- */
-debug('configuring middleware')
-app.use(logger('dev'))
-app.use(bodyParser.json())
-
-/**
- * API routes / endpoints
- */
-app.use('/todos', (req, res) => {
-  // console.log(req.body)
-  let todo = new Todo({
-    text: req.body.text
-  })
-  todo
-    .save()
-    .then((doc) => res.send(doc))
-    .catch(err => res.status(400).send(err))
-})
+const PORT = 3000
+const HOST = 'localhost'
 
 // console.dir(debug, { colors: true, depth: 2 })
-app.listen(3000, () => {
-  debug('Server started on port 3000')
+app.listen(PORT, HOST, () => {
+  debug('Starting server')
+  console.log(`
+    Server Started
+    =============================
+    host : ${HOST}
+    Port : ${PORT}
+    -----------------------------
+ `)
 })
