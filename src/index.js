560412268f55230ff15e05149f0088faa76212b9
Pepeye <gaofolaju@gmail.com>
2017-02-18 17:15:38 +0000

    moved server back to src/index, export + completed POST /todos test

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
-const app = require('./server')
-const debug = require('debug')('stormtrooper:server')
+const express = require('express')
+const bodyParser = require('body-parser')
+const logger = require('morgan')
+let debug = require('debug')('stormtrooper')
+
+// const mongoose = require('./lib/db')
+const { Todo } = require('./models')
 
 const PORT = 3000
 const HOST = 'localhost'
+
+let app = express()
+
+/**
+ * Configure middleware
+ */
+debug('configuring middleware')
+app.use(logger('dev'))
+app.use(bodyParser.json())
+
+/**
+ * API routes / endpoints
+ */
+app.use('/todos', (req, res) => {
+  // console.log(req.body)
+  let todo = new Todo({
+    text: req.body.text
+  })
+  todo
+    .save()
+    .then((doc) => res.send(doc))
+    .catch(err => res.status(400).send(err))
+})
 
 // console.dir(debug, { colors: true, depth: 2 })
 app.listen(PORT, HOST, () => {
@@ -15,3 +43,5 @@ app.listen(PORT, HOST, () => {
     -----------------------------
  `)
 })
+
+module.exports = { app }
--- src/tests/server.test.js
+++ src/tests/server.test.js
@@ -1,1 +1,30 @@
-const supertest = require('supertest')
+const request = require('supertest')
+const { app } = require('../')
+const { Todo } = require('../models')
+
+describe('POST /todos', () => {
+  test('should create a new todo', (done) => {
+    let text = 'Todo test'
+
+    request(app)
+      .post('/todos')
+      .send({ text })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.text).toBe(text)
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err)
+        }
+
+        Todo
+          .find({ text })
+          .then((todos) => {
+            expect(todos.length).toBe(1)
+            expect(todos[0].text).toBe(text)
+            done()
+          })
+      })
+  })
+})
