ada9385cd0744938da9add2544b3c9567bcfce22
Pepeye <gaofolaju@gmail.com>
2017-02-19 21:12:17 +0000

    cleaned up server comments, renamed todo tests

--- src/server.js
+++ src/server.js
@@ -32,33 +32,6 @@ app.use('/', routes)
 /**
  * API routes / endpoints
  */
-// app.post('/todos', (req, res) => {
-//   // console.log(req.body)
-//   let todo = new Todo({
-//     text: req.body.text
-//   })
-//   todo
-//     .save()
-//     .then((doc) => res.send(doc))
-//     .catch(err => res.status(400).send(err))
-// })
-//
-// app.get('/todos', (req, res) => {
-//   Todo
-//     .find()
-//     .then((todos) => {
-//       res.send({ todos })
-//     })
-//     .catch(err => res.status(400).send(err))
-// })
-//
-// app.post('/users', (req, res) => {
-//   console.dir(req.body)
-//   let user = new User(req.body)
-//   user
-//     .save()
-//     .then((doc) => res.send(doc))
-//     .catch(err => res.status(400).send(err))
-// })
+app.use('/', routes)
 
 module.exports = app
