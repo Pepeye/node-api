26a58fced55382b50f82afc538a1066d07fd0a8b
Pepeye <gaofolaju@gmail.com>
2017-02-27 23:18:11 +0000

    added user tests, refractored todo tests

--- src/todos/router.js
+++ src/todos/router.js
@@ -9,6 +9,6 @@ router.route('/')
 router.route('/:id')
   .put((...args) => controller.update(...args))
   .get((...args) => controller.findById(...args))
-  .delete((...args) => controller.remove(...args))
+  .delete((...args) => controller.delete(...args))
 
 module.exports = router
